09e7b513d1603c2471c72a45da1d9463be859662
alokpatle <75228303+alokpatle@users.noreply.github.com>
2025-06-24 20:36:16 -0600

    UAT Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-import "tfplan"
-
-main = rule {
-  all tfplan.resources.aws*instance as *, instances {
-    all instances as \_, r {
-      (length(r.applied.tags) else 0) > 0
-    }
-  }
-}
-
 terraform {
   required_providers {
     aws = {
@@ -24,7 +14,4 @@ provider "aws" {
 resource "aws_instance" "test_server" {
   ami           = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
- tags = {
-    Name = "Test server2"
-  }
 }
